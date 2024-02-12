9743dd225f787e45cd1df92b0aa71bf3d06d80e3
Preeth02 <preethampreetham27@gmail.com>
2024-02-12 20:28:30 +0530

    Connecting with DataBase

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectingDB = async () => {
+    try {
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n Connection successful with MONGO_DB !!! DB HOST: ${connectInstance.connection.host}`)
+    } catch (err) {
+        console.log("ERROR: FALIED TO CONNECT WITH MONGO_DB", err)
+        process.exit(1)
+    }
+}
+export default connectingDB;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import dotenv from "dotenv"
+import connectingDB from "./db/index.js"
+dotenv.config({ path: "./env" })
+connectingDB()
