04bf18a63b2012667c1b46d88f7cd9291dc6799d
Preeth02 <preethampreetham27@gmail.com>
2024-03-03 10:40:59 +0530

    User register route and a user controller setup

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+// Routes declaration..
+// app.use("/users", userRouter)          ***This can be used but more standard way practice is as follows***
+app.use("/api/v1/users", userRouter)
+
+
 export default app
